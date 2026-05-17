cab8c48b4345b9a3e9435afdb56ccdd094d030c4
Ng Chun Sing <r14922186@ntu.edu.tw>
2026-05-17 07:06:41 +0000

    Revert "demo typecheck failure"
    This reverts commit 1cbae4b4ab43e9961fc9795aabe95bc3365a6569.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
-const x: number = "not a number";
-
 export function buildApp(options: FastifyServerOptions = {}) {
   const app = Fastify({
     logger: options.logger ?? true,
