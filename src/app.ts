1cbae4b4ab43e9961fc9795aabe95bc3365a6569
Ng Chun Sing <r14922186@ntu.edu.tw>
2026-05-17 07:02:17 +0000

    demo typecheck failure

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
+const x: number = "not a number";
+
 export function buildApp(options: FastifyServerOptions = {}) {
   const app = Fastify({
     logger: options.logger ?? true,
